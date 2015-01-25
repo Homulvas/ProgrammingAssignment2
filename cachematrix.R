16475a9e66820a22a7aaeaa136d8855bd01ec601
Ignas Kaziukenas <ignas@ignas.co>
2015-01-25 22:16:03 +0200

    Completed makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matirx construct that has setters and getters for the matrix itself and
+## the inverse of the matrix
 
-## Write a short comment describing this function
+## The code is copied from makeVector example with certain changes to the names
+## of the variables to fit this task better
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## the cached inverse value
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    ## set and get inverse value
+    setinverse <- function(solved) inverse <<- solved
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
